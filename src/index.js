fd14234ed52c14fcbe6d2d334b00a9c19c8cd7fe
KiHoon Roh <started677@gmail.com>
2022-08-24 17:58:19 +0900

    [fix]baseUrl 변경

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/wanted-pre-onboarding-fe/">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <AuthContextProvider>
                 <TodoContextProvider>
                     <ModalContextProvider>
